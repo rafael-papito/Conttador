07405060a704eccd9140f5c4c0c4cdad83fef866
Rafael Porto <rafaelgportof@gmail.com>
2021-08-31 09:26:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function contar(){
-    
+    var inicio = document.getElementById('ini')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('pas')
+    var res = document.getElementById('res')
 }
