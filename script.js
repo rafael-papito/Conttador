c4a9190a2eef3b49a7c525145d55e1eee06d5703
Rafael Porto <rafaelgportof@gmail.com>
2021-08-31 09:28:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ function contar(){
     var fim = document.getElementById('fim')
     var passo = document.getElementById('pas')
     var res = document.getElementById('res')
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert("[ERRO] Faltam dados!")
+    }else{
+        
+    }
 }
