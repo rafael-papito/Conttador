0388c2c2ee52dc76edb29ad0c3fc7e77dddeb1d5
Rafael Porto <rafaelgportof@gmail.com>
2021-09-01 08:30:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ function contar(){
     if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         alert("[ERRO] Faltam dados!")
     }else{
-        
+        res.innerHTML=`<p>Contando:</p>`
+        var i = Number(inicio.value)
+        var f=  Number(fim.value)
+        var p = Number(passo.value)
+        if(p <= 0){
+            alert("Passo Invalido! Considerando 1")
+            p = 1
+        }
     }
 }
